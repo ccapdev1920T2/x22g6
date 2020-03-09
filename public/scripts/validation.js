6fff30cb9899cfd77411019eec7cc44df81d6368
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-10 07:34:00 +0800

    Combined textbox.js to validation.js

--- public/scripts/validation.js
+++ public/scripts/validation.js
@@ -33,6 +33,31 @@ $(document).ready(function(){
 		checkID(e);
 		checkPassword(e);		
 	});
+
+	$(".numonly").keypress(function(e){
+		var key = e.keyCode;
+		if(!(key >= 48 && key <= 57)){
+			e.preventDefault();
+		}
+	});
+
+	$(".textonly").keypress(function(e){
+		var key = e.keyCode;
+		if(key >= 48 && key <= 57){
+			e.preventDefault();
+		}
+	});
+
+	$(".required").blur(function(){
+
+		if($(this).val() == ""){
+			$(this).css("border-color", "red");
+		}
+
+		else{
+			$(this).css("border-color", "#707070")
+		}
+	});
 });
 function checkEmail(e){
 	var email = $("#email").val();
