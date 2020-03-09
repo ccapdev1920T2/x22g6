e3a6018aef5c13a0f712b45a75940345de57558d
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-10 07:40:00 +0800

    Added edit-profile.js
    Scripts related to editing the profile in validation.js has been transferred to edit-profile.js

---
+++ public/scripts/edit-profile.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#cancel_profile").click(function(e){
+		$("#edit_profile").html("Edit Profile");
+		$("#change_password").show();
+		$("#form__profile input").attr('disabled', 'disabled');
+		$("#cancel_profile").hide();
+		e.preventDefault();
+	});
+
+	$("#change-password").click(function(e){
+		checkPassword(e);
+	});
+
+	$("#edit_profile").click(function(e){
+		if($("#edit_profile").text() == "Edit Profile"){
+			$("#edit_profile").html("Save");
+			$("#form__profile input").removeAttr('disabled');
+			$("#change_password").hide();
+			$("#id-number").attr('disabled', 'disabled');
+			$("#cancel_profile").show();
+			e.preventDefault();
+		}
+
+		else{
+			checkText(e);
+			checkEmail(e);
+		}
+	});
+
+	$("#register").click(function(e){
+		checkText(e);
+		checkEmail(e);
+		checkID(e);
+		checkPassword(e);		
+	});
+})
