ab15ebd0953188b92073db1c83f9b2b638fa4b0f
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-10 22:27:29 +0800

    Server responds to POST request /change-password
    The server temporarily sends an HTTP response with status code 200

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -6,4 +6,9 @@ module.exports = function(app){
     app.post("/profile", function(req, res){
     	res.render("profile");
     });
+
+    app.post("/change-password", function(req, res){
+        res.status(200);
+        res.end();
+    });
 }
