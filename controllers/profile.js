186a4823157b33e80e3acdc3238bb640219bdc9f
Mike Vincent Quito <mike_vincent_quito@dlsu.edu.ph>
2020-03-10 15:50:01 +0800

    register-student.hbs and profile.hbs now redirects after clicking Submit or Save

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,4 +2,8 @@ module.exports = function(app){
     app.get("/profile", function(req, res){
         res.render("profile");
     });
+
+    app.post("/profile", function(req, res){
+    	res.render("profile");
+    });
 }
