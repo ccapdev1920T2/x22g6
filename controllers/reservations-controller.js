d152873ee88733fcdbd5143c9dbaaae5055e00eb
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-12 14:32:55 +0800

    Server responds to GET request /location/:location Response is an array of time slots based on the value of the location

--- controllers/reservations-controller.js
+++ controllers/reservations-controller.js
@@ -1,3 +1,23 @@
+// Temporary storage for time slots
+const LAG_TO_MNL_TIME_SLOTS = [
+    {value: "0545", presentation: "5:45 AM"},
+    {value: "0700", presentation: "7:00 AM"},
+    {value: "0730", presentation: "7:30 AM"},
+    {value: "0900", presentation: "9:00 AM"},
+    {value: "1100", presentation: "11:00 AM"},
+    {value: "1300", presentation: "1:00 PM"},
+]
+
+// Temporary storage for time slots
+const MNL_TO_LAG_TIME_SLOTS = [
+    {value: "0600", presentation: "6:00 AM"},
+    {value: "0730", presentation: "7:30 AM"},
+    {value: "0930", presentation: "9:30 AM"},
+    {value: "1100", presentation: "11:00 AM"},
+    {value: "1300", presentation: "1:00 PM"},
+    {value: "1430", presentation: "2:30 PM"},
+]
+
 module.exports = function(app){
     app.get("/my-reservations", function(req, res){
         res.render("my-reservations");
@@ -5,6 +25,21 @@ module.exports = function(app){
 
     app.get("/user-reservations", function(req, res){
         res.render("user-reservations");
+    });
+
+    //For handling change in reservation time
+    app.get("/location/:location", function(req, res){
+        switch(req.params.location){
+            case "MNL-to-LAG":
+                res.send(MNL_TO_LAG_TIME_SLOTS);
+                break;
+            case "LAG-to-MNL":
+                res.send(LAG_TO_MNL_TIME_SLOTS);
+                break;
+            default:
+                res.status(400);
+                res.end();
+        }
     });
     
     // For handling check ins from staff
