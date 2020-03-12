2817cee28aa6f6a5f95b4b74dc6c9c8fa56ef35d
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-12 21:53:12 +0800

    AJAX request  student registration can be sent
    Upon submitting the form for student-registration, a POST request to route /register-student is sent to the server

---
+++ public/scripts/register-student.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    let studenRegistrationForm = $("#student-registration-form");
+    $("#student-registration-form button[type=\"submit\"]").click(function(e){
+        e.preventDefault();
+        // Validates Inputs
+        let isValid = Validator.checkRequired(studenRegistrationForm) &&
+            Validator.checkEmail($("#email")) && Validator.checkID($("#id-number")) &&
+            Validator.checkEqual($("#password"), $("#confirm-password"));
+        if(isValid){
+            // POST request to register student
+            $.ajax({
+                type: "POST",
+                url: "register-student",
+                data: studenRegistrationForm.serialize(),
+                success: function(){
+                    Modal.displayModalMessage(`You're account has been created.  Click <a href="/login">here</a> to login`);
+                },
+                error: function(jqxhr){
+                    Modal.displayModalMessage(jqxhr.responseText);
+                },
+                complete: function(){
+                    console.log(studenRegistrationForm.children("input"));
+                    $(studenRegistrationForm.find("input")).each(function(){
+                        $(this).val("");
+                    });
+                }
+            })
+        }
+    })
+});
