179ae15656366575ed99a968cc5b03491f11763d
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-12 11:53:45 +0800

    Server responds to POST request route /check-in
    Temporarily sends an HTTP response with status code 200

--- controllers/reservations-controller.js
+++ controllers/reservations-controller.js
@@ -6,4 +6,10 @@ module.exports = function(app){
     app.get("/user-reservations", function(req, res){
         res.render("user-reservations");
     });
+    
+    // For handling check ins from staff
+    app.post("/check-in", function(req, res){
+        res.status(200);
+        res.end();
+    });
 }
