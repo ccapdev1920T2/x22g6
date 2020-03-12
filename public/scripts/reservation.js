f751aea9d837bc0d7a0213222ce3f200949b74ec
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-12 14:50:44 +0800

    Reservation modal time selector option changes
    Upon changing the choice for Source and Destination, the options for time selector changes through sending a GET req to server on route /location/:location

---
+++ public/scripts/reservation.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	let timeSelector = $("#reservation-time");
+	let locationSelector = $("#reservation-location");
+
+	console.log("test");
+	locationSelector.change(function(){
+		$.ajax({
+			type: "GET",
+			url: "/location/" + locationSelector.val(),
+			success: function(data){
+				timeSelector.children().remove();
+				for(let i=0; i<data.length; ++i){
+					let text = document.createTextNode(data[i].presentation);
+					timeSelector.append($("<option>").attr("value", data[i].value).append(text));
+				}
+			}
+		});
+	});
+});
