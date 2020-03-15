f44599f72cee0a6a96aa835d534cda3566380195
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-15 11:47:55 +0800

    AJAX req can be sent in schedule page
    Upon changing the filter options, A GET req is sent with url /schedule/:date/:location.  Server temporarily sends a res with status code 200

--- controllers/arrows-schedule-controller.js
+++ controllers/arrows-schedule-controller.js
@@ -2,4 +2,9 @@ module.exports = function(app){
     app.get("/schedule", function(req, res){
         res.render("schedule");
     });
+
+    app.get("/schedule/:date/:path", function(req, res){
+        res.status(200);
+        res.end();
+    });
 }
