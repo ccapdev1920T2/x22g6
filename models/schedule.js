22e45740b9bb4a01224589c1e0cb902b27ee7549
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-28 16:50:20 +0800

    Added an instance method to the schedule model
    The method returns the 12-hour format of a schedule's time

--- models/schedule.js
+++ models/schedule.js
@@ -24,6 +24,17 @@ const scheduleSchema = new Schema({
     }
 });
 
+/**************Schedule model instance methods *********************/
+// Returns a string that represents the time of the schedule using the 12-hour clock
+scheduleSchema.methods.get12HourFormat = function(){
+    let hour = Math.floor(this.time / 100);
+    let minute = this.time % 100;
+    let suffix = (hour >= 12) ? "P.M." : "A.M.";
+    hour = (hour > 12) ? hour - 12: hour;
+    minute = (minute === 0) ? minute + "0" : minute;
+    return hour + ":" + minute + " " + suffix;
+}
+
 const Schedule = mongoose.model("Schedule", scheduelSchema);
 
 module.exports = Schedule;
