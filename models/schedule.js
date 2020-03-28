4d3a6924357b402bbb55cca9e9bc1e3e17e31f54
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-03-28 16:11:10 +0800

    Added constants to schedule model

--- models/schedule.js
+++ models/schedule.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const LAG = "LAG";
+const MNL = "MNL";
+
 const scheduleSchema = new Schema({
     origin: {
         type: String,
-        enum: ['Laguna', 'Manila'],
+        enum: [LAG, MNL],
         required: true
     },
 
     destination: {
         type: String,
-        enum: ['Laguna', 'Manila'],
+        enum: [LAG, MNL],
         required: true
     },
 
