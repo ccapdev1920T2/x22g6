dd4ff8d55023e3818bd21071c7639cb7c34f1e52
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-04 14:34:06 +0800

    schedule page now dependent on db

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    $("#schedule-filters input, #schedule-filters selector").change(function(){
+    $("#schedule-filters input, #schedule-filters select").change(function(){
         $.ajax({
             type: "GET",
             url: "/schedule/filter/" + $("#schedule-filters__date").val() + "/" + $("#schedule-filters__trip").val(),
-            success: function(){
-                
+            success: function(data){
+                let tableBody = $("#schedule-table").find(".table__body");
+                tableBody.children("tr").remove();
+                for(let i=0; i<data.length; ++i){
+                    let timeEntry = $("<td>").html(data[i].time);
+                    let slotEntry = $("<td>").html(data[i].openSlots + "/25");
+                    let row = $("<tr>").append(timeEntry).append(slotEntry);
+                    tableBody.append(row);
+                }
+            },
+            error: function(jqxhr){
+                console.log(jqxhr.responseText);
             }
         });
     });
+
+    $("#schedule-filters input").trigger("change");
 })
