4d131188d550394e0effd01666a8cc541008c24e
Mike Vincent Quito <mike_vincent_quito@dlsu.edu.ph>
2020-04-04 11:25:21 +0800

    Removed Professor from routes register-routes.js

--- routes/register-routes.js
+++ routes/register-routes.js
@@ -11,10 +11,4 @@ router.get("/student", userController.sendStudentRegistrationPage);
 // POST request for registering a new student
 router.post("/student", userController.registerStudent);
 
-// GET request for professor registration page
-router.get("/professor", auth.checkStaff, auth.getUserInfo, userController.sendProfessorRegistrationPage);
-
-// POST request for registering a new professor
-router.post("/professor", auth.checkStaff, userController.registerProfessor);
-
 module.exports = router;
