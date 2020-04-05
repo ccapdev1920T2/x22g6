6cdce9ca44c1db5aec08c3fd528f1f02c6bfe4c2
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-05 16:26:44 +0800

    schedule table shows message when weekend is selected

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -4,11 +4,18 @@ $(document).ready(function(){
         let table = $("#schedule-table");
         let tableBody = table.find(".table__body");
         tableBody.find("tr").remove();
+        let dateInput = $("#schedule-filters__date").val();
+        let date = new Date(dateInput + "T00:00:00");
+        if(date.getDay() === 0 || date.getDay() === 6){
+            Table.editMessage("No reservations on weekends");
+            Table.showMessage(table);
+            return;
+        }
         Table.editMessage("Retrieving Data...");
         Table.showMessage(table);
         $.ajax({
             type: "GET",
-            url: "/schedule/filter/" + $("#schedule-filters__date").val() + "/" + $("#schedule-filters__trip").val(),
+            url: "/schedule/filter/" + dateInput + "/" + $("#schedule-filters__trip").val(),
             success: function(data){
                 Table.removeMessage();
                 for(let i=0; i<data.length; ++i){
