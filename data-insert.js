bec9ef66082acfc522ad0ce24a24c480c65ca45f
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-05 17:30:48 +0800

    Updated data-insert.js

--- data-insert.js
+++ data-insert.js
@@ -41,12 +41,24 @@ const dbOptions = {
         await new Schedule({time: 1530, origin: Schedule.MNL, destination: Schedule.LAG}).save();
         await new Schedule({time: 1700, origin: Schedule.MNL, destination: Schedule.LAG}).save();
         await new Schedule({time: 1815, origin: Schedule.MNL, destination: Schedule.LAG}).save();
-
         console.log("Inserted schedule documents.");
+
         await User.createUser(11826532, "Mike", "Quito", "mike_quito@dlsu.edu.ph", "headphones", User.STAFF_TYPE);
         await User.createUser(11825647, "Joshua", "Gaurano", "joshua_gaurano@dlsu.edu.ph", "mousepad", User.STUDENT_TYPE);
         await User.createUser(11829672, "JJ", "Reyes", "jj_reyes@dlsu.edu.ph", "computers", User.STUDENT_TYPE);
         console.log("Inserted user documents.");
+
+        await Reservation.createReservation(11829672, "2020-04-07", "LAG-to-MNL", 700);
+        await Reservation.createReservation(11829672, "2020-04-07", "MNL-to-LAG", 1700);
+        await Reservation.createReservation(11829672, "2020-04-29", "MNL-to-LAG", 1530);
+        await Reservation.createReservation(11829672, "2020-05-13", "LAG-to-MNL", 1100);
+        await Reservation.createReservation(11829672, "2020-05-13", "MNL-to-LAG", 1300);
+        await Reservation.createReservation(11829672, "2020-05-14", "LAG-to-MNL", 900);
+        await Reservation.createReservation(11829672, "2020-05-14", "MNL-to-LAG", 1815);
+        console.log("Inserted reservation documents.");
+
+        await new SuspendedUsers({userId: 11825647}).save();
+        console.log("Inserted suspended-user documents.");
 
         console.log("Data successfuly inserted.");
         process.exit()
