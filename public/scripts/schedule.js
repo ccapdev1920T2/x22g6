64774a87caa7f6c4477a651293be90bdc26b7f15
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-05 11:27:22 +0800

    Schedule table now shows retrieval message

--- public/scripts/schedule.js
+++ public/scripts/schedule.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
     $("#schedule-filters input, #schedule-filters select").change(function(){
+        let tableBody = $("#schedule-table").find(".table__body");
+        let retrievalMessage = $("<p>").text("Retrieving data...").addClass("table__message");
+        tableBody.children("tr").remove();
+        retrievalMessage.insertAfter(tableBody.closest("table"));
         $.ajax({
             type: "GET",
             url: "/schedule/filter/" + $("#schedule-filters__date").val() + "/" + $("#schedule-filters__trip").val(),
             success: function(data){
-                let tableBody = $("#schedule-table").find(".table__body");
-                tableBody.children("tr").remove();
+                retrievalMessage.remove();
                 for(let i=0; i<data.length; ++i){
                     let timeEntry = $("<td>").html(data[i].time);
                     let slotEntry = $("<td>").html(data[i].openSlots);
@@ -14,7 +17,7 @@ $(document).ready(function(){
                 }
             },
             error: function(jqxhr){
-                console.log(jqxhr.responseText);
+               retrievalMessage.text("Cannot retrieve data at this time");
             }
         });
     });
