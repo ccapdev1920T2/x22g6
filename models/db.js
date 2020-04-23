77c4217b3512f98f2eef6167cc2f9bec9b92c5a5
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-23 20:43:12 +0800

    Modified db.js module interface

--- models/db.js
+++ models/db.js
@@ -11,6 +11,8 @@ module.exports.connect = function(){
     return mongoose.connect(uri, options);
 }
 
+module.exports.connection = mongoose.connection;
+
 mongoose.connection.on("connected", () => console.log("Connected to MongoDB server"));
 mongoose.connection.on("reconnected", () => console.log("Reconnected to MongoDB server"));
 mongoose.connection.on("disconnected", () => console.log("Disconnected from MongoDB server"));
