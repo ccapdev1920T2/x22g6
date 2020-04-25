fc9ce151e0a0acabf18ad50b37df5f51f6d399aa
Jj Reyes <47908236+jjbreyes@users.noreply.github.com>
2020-04-25 16:28:19 +0800

    Update suspended-user-controller.js

--- controllers/suspended-user-controller.js
+++ controllers/suspended-user-controller.js
@@ -2,8 +2,20 @@ const SuspendedUser = require("../models/suspended-user-model");
 
 exports.ScheduledDeletion = async function(req, res){
     try{
-        await SuspendedUser.find()
+         let suspended = await SuspendedUser.findOne({userId: req.signedCookies.id});
+        // Checks if the suspension is already lifted and removes it if it already is
+        if(suspended && suspended.releaseDate.getTime() == (new Date()).getTime()){
+            await suspended.remove()
+            suspended = null;
+        }
     }catch(err){
         res.status(500).send("Cannot suspend user at this schedule.");
     }
 }
+
+var schedule = require('node-schedule');
+var date = new Date(2012, 11, 21, 5, 30, 0);
+ 
+var j = schedule.scheduleJob(date, function(){
+  console.log('The world is going to end today.');
+});
