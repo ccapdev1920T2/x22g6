45963407c161855eaddb3f18a421063757570814
Jj Reyes <47908236+jjbreyes@users.noreply.github.com>
2020-04-25 15:52:07 +0800

    Update suspended-user-controller.js

--- controllers/suspended-user-controller.js
+++ controllers/suspended-user-controller.js
@@ -2,4 +2,10 @@ const SuspendedUser = require("../models/suspended-user-model");
 
 let SuspendedUserCollection = suspendedUsers.find();
 
-exports.scheduledDeletion
+exports.ScheduledDeletion = async function(req, res){
+    try{
+        
+    }catch(err){
+        res.status(500).send("Cannot suspend user at this schedule.");
+    }
+}
