7652a4cc724931b08b58f8f5394f5fec9c6e121d
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-26 12:44:31 +0800

    Added callback in remove method in schedulReleaseDate

--- controllers/suspended-user-controller.js
+++ controllers/suspended-user-controller.js
@@ -6,8 +6,14 @@ const schedule = require("node-schedule");
          let suspended = await SuspendedUser.find();
         for(let i=0; i<suspended.length; i++){
             schedule.scheduleJob(suspended[i].releaseDate, function(){
-                suspended[i].remove();
-            }
+                suspended[i].remove(function(err, product){
+                    console.log(product);
+                    if(err)
+                        console.log(`Cannot lift suspension of ${suspended[i]}: ${err}`);
+                    else
+                        console.log(`Lifted suspension of ${suspended[i]}`);
+                });
+            });
         }
     }catch(err){
         console.log(err);
