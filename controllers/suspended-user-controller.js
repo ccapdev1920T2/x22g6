c3f4b1e795aa4ad8e9039ac579ee8254892a91c0
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-26 14:54:51 +0800

    suspended-user-controller now uses liftSuspension

--- controllers/suspended-user-controller.js
+++ controllers/suspended-user-controller.js
@@ -1,22 +1,21 @@
 const SuspendedUser = require("../models/suspended-user-model");
 const schedule = require("node-schedule");
 //Schedules deletion of the suspension of suspended users
- async function ScheduledDeletion(){
+async function scheduleReleaseDate(){
     try{
-         let suspended = await SuspendedUser.find();
+        let suspended = await SuspendedUser.find();
         for(let i=0; i<suspended.length; i++){
             schedule.scheduleJob(suspended[i].releaseDate, function(){
-                suspended[i].remove(function(err, product){
-                    console.log(product);
-                    if(err)
-                        console.log(`Cannot lift suspension of ${suspended[i]}: ${err}`);
-                    else
-                        console.log(`Lifted suspension of ${suspended[i]}`);
+                suspended[i].liftSuspension().then(function(value){
+                    console.log(`Lifted suspension of ${suspended[i]}`);
+                }, function(err){
+                    console.log(`Cannot lift suspension of ${suspended[i]}: ${err}`);
                 });
             });
         }
+        console.log("Scheduled release date of existing suspended users");
     }catch(err){
-        console.log(err);
+        console.log("Cannot schedule release date of existing suspended users :" + err);
     }
 }
-ScheduledDeletion();
+scheduleReleaseDate();
