bddc838300d966219a48fd2878d3d54a5b5e33fe
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2020-04-27 19:12:51 +0800

    Lifts suspensions whose release date has passed on start up

--- controllers/suspended-user-controller.js
+++ controllers/suspended-user-controller.js
@@ -5,9 +5,14 @@ module.exports.scheduleReleaseDate =  async function scheduleReleaseDate(){
     try{
         let suspended = await SuspendedUser.find();
         for(let i=0; i<suspended.length; i++){
-            schedule.scheduleJob(suspended[i].releaseDate, function(){
+            let today = new Date();
+            if(suspended[i].releaseDate.getTime() <= today.getTime())
                 suspended[i].liftSuspension();
-            });
+            else{
+                schedule.scheduleJob(suspended[i].releaseDate, function(){
+                    suspended[i].liftSuspension();
+                });
+            }
         }
         console.log("Scheduled release date of existing suspended users");
     }catch(err){
